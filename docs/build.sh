fe2bb15389286239c946ea22d0f5490e55d86673
max <max@gmail>
2022-03-01 21:45:47 -0500

    upgrading for simple use

--- docs/build.sh
+++ docs/build.sh
@@ -2,5 +2,5 @@ mainCFG="page-main.conf"
 otherCFG="page-other.conf"
 
 python ../jemdoc -c ${mainCFG}  index.jemdoc
-python ../jemdoc -c ${otherCFG}  pages/teaching.jemdoc
+#python ../jemdoc -c ${otherCFG}  pages/teaching.jemdoc
 python ../jemdoc -c ${otherCFG}  pages/publications.jemdoc
