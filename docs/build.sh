9914b1db8d2dac788579691f07f744b662ef6287
max <max@gmail>
2022-06-07 09:43:36 -0400

    update blog information

--- docs/build.sh
+++ docs/build.sh
@@ -4,3 +4,5 @@ otherCFG="page-other.conf"
 python ../jemdoc -c ${mainCFG}  index.jemdoc
 #python ../jemdoc -c ${otherCFG}  pages/teaching.jemdoc
 python ../jemdoc -c ${otherCFG}  pages/publications.jemdoc
+python ../jemdoc -c ${otherCFG}  pages/blog.jemdoc
+python ../jemdoc -c ${otherCFG}  pages/inv_fair.jemdoc
