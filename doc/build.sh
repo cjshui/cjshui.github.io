1396102951bb1ab2ce65ff11aaa66908af75f348
max <max@gmail>
2023-05-28 12:43:46 -0400

    fix tiny bug

--- doc/build.sh
+++ doc/build.sh
@@ -1,7 +1,6 @@
 mainCFG="mysite.conf"
 otherCFG="mysite.conf"
-#mainCFG="page-main.conf"
-#otherCFG="page-other.conf"
+
 
 python jemdoc -c ${mainCFG}  -o ../docs/   index.jemdoc
 python jemdoc -c ${otherCFG} -o ../docs/   publications.jemdoc
