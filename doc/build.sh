31bc9e1492f94962e161ff31b514d052e6285fa3
max <max@gmail>
2023-10-03 12:34:20 -0400

    update

--- doc/build.sh
+++ doc/build.sh
@@ -6,3 +6,4 @@ python jemdoc -c ${mainCFG}  -o ../docs/   index.jemdoc
 python jemdoc -c ${otherCFG} -o ../docs/   publications.jemdoc
 python jemdoc -c ${otherCFG} -o ../docs/   blog.jemdoc
 python jemdoc -c ${otherCFG} -o ../docs/   inv_fair.jemdoc
+python jemdoc -c ${otherCFG} -o ../docs/   note.jemdoc
