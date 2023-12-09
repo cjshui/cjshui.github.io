1b4924a4c00b7e8075ffdf7fd1e579d7e0471ac7
max <max@gmail>
2023-12-09 15:05:19 -0500

    update/optimize website

--- doc/build.sh
+++ doc/build.sh
@@ -4,6 +4,7 @@ otherCFG="mysite.conf"
 
 python jemdoc -c ${mainCFG}  -o ../docs/   index.jemdoc
 python jemdoc -c ${otherCFG} -o ../docs/   publications.jemdoc
-python jemdoc -c ${otherCFG} -o ../docs/   blog.jemdoc
-python jemdoc -c ${otherCFG} -o ../docs/   inv_fair.jemdoc
-python jemdoc -c ${otherCFG} -o ../docs/   note.jemdoc
+python jemdoc -c ${otherCFG} -o ../docs/   news.jemdoc
+#python jemdoc -c ${otherCFG} -o ../docs/   blog.jemdoc
+#python jemdoc -c ${otherCFG} -o ../docs/   inv_fair.jemdoc
+#python jemdoc -c ${otherCFG} -o ../docs/   note.jemdoc
